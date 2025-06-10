5aab09e8ebf4ef2d37f31cc45ad5961838df2927
Takagi-Masamune-TCI <Takagi.Masamune@trans-cosmos.co.jp>
2025-06-10 11:36:55 +0900

    add Task pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// トップページはタスクリストの「一覧」と同じページにルーティングする設定を入れる
+Route::get('/', [TaskController::class, 'index']);
+
+// タスクを管理するRouterには Route::resource を利用する
+Route::resource('tasks', TaskController::class);
